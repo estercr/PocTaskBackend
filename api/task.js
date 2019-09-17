02c0414e0098494578de270f1c89f1912e725c64
Ester Cristina <ester.cristina.silva@accenture.com>
2019-09-17 10:08:01 -0300

    adjust task

--- api/task.js
+++ api/task.js
@@ -2,9 +2,7 @@ const moment = require('moment')
 
 module.exports = app => {
     const getTasks = (req, res) => {
-        const date = req.query.date ? req.query.date
-            : moment().endOf('day').toDate()
-
+        const date = req.query.date ? req.query.date : moment().endOf('day').toDate()
         app.db('tasks')
             .where({ userId: req.user.id })
             .where('estimateAt', '<=', date)
